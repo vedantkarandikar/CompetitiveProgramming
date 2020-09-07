d0cebf938852e4095e55c1b96aa73f492c016d41
Vedant <vedantkarandikar@gmail.com>
2020-09-07 20:23:03 +0530

    Add DS

---
+++ Data Structures/Tree/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+struct node {
+    int val = 0;
+    node* left = nullptr;
+    node* right = nullptr;
+    node(int val, node* left, node* right) {
+	this->val = val;
+	this->left = left;
+	this->right = right;
+    }
+};
+
+
+int main() {
+    return 0;
+}
